d40a70c387464201eb9bf9c963fab1a7f4e2fc61
Wilmar Medeiros <wilmarpm1311@gmail.com>
2025-05-12 23:35:59 -0300

    conectando PaginaGeradorEstampa como página principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import EstampaGenerator from "../components/EstampaGenerator";
+
+import dynamic from 'next/dynamic';
+
+const PaginaGeradorEstampa = dynamic(() => import('../../PaginaGeradorEstampa'), { ssr: false });
 
 export default function Home() {
-  return <EstampaGenerator />;
+  return <PaginaGeradorEstampa />;
 }
