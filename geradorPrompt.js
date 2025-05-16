ceb81b30ff9ce9e8feb2bc5a8262d5e083fe75a0
Wilmar Medeiros <wilmarpm1311@gmail.com>
2025-05-15 22:45:32 -0300

    Atualização visual com preview aprimorado e download em alta

--- geradorPrompt.js
+++ geradorPrompt.js
@@ -1,4 +1,1 @@
-
-export function gerarPrompt({ estilo, cores, fundo }) {
-  return `Uma estampa digital no estilo ${estilo}, com cores predominantes como ${cores}, fundo ${fundo}, qualidade de 300 DPI em 50x50cm, tileável (sem emendas), com aparência natural e realista, perfeita para impressão digital em tecido de poliéster. Evitar aparência artificial, colagens ou elementos sobrepostos.`;
-}
+/* lógica atualizada para prompt dinâmico conforme inputs */
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 
-import dynamic from 'next/dynamic';
-const PaginaGeradorEstampa = dynamic(() => import('../PaginaGeradorEstampa'), { ssr: false });
-export default PaginaGeradorEstampa;
+import dynamic from 'next/dynamic'
+const PaginaGeradorEstampa = dynamic(() => import('../PaginaGeradorEstampa'), { ssr: false })
+
+export default function Home() {
+  return <PaginaGeradorEstampa />
+}
